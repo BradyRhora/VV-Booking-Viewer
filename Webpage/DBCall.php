da972268007b09947dae99240362acfa4e2bd016
Brady Rhora <brady0423@gmail.com>
2021-05-07 17:21:27 -0400

    not sure tbh

--- Webpage/DBCall.php
+++ Webpage/DBCall.php
@@ -1,5 +1,9 @@
 
 <?php
+    //ini_set('display_errors', 1);
+    //ini_set('display_startup_errors', 1);
+    //error_reporting(E_ALL);
+
     class MyDB extends SQLite3 {
         function __construct() {
         $this->open('../Databases.db');
